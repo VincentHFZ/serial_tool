2a6e8465fcd614a94a417e833aafb7c1196b79eb
Vincnet <534050016@qq.com>
2020-11-19 18:32:08 +0800

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * 实现功能
+ *
+ * 1. 使能串口，将接收到的串口数据写入到文件中
+ * 2. 启用两个线程，线程1负责获取串口数据，线程2负责定时将
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "serial/serial.h"
+
+int main()
+{
+    std::string port;
+    uint32_t baud;
+
+    return 0;
+}
+
